0422da0ca39d9a43c8c20418eb1009064d370480
Jacek Bilski <jacek.bilski@innoq.com>
2022-05-12 18:29:38 +0200

    Moving DB schema initialization outside the tests themselves (JDBC only).

--- src/main/java/tech/bilski/playground/sql/EntityWithArray.java
+++ src/main/java/tech/bilski/playground/sql/EntityWithArray.java
@@ -1,7 +1,9 @@
 package tech.bilski.playground.sql;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table("entity_with_array")
 public class EntityWithArray {
     @Id
     Long id;
--- src/main/java/tech/bilski/playground/sql/EntityWithList.java
+++ src/main/java/tech/bilski/playground/sql/EntityWithList.java
@@ -1,9 +1,11 @@
 package tech.bilski.playground.sql;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.List;
 
+@Table("entity_with_list")
 public class EntityWithList {
     @Id
     Long id;
